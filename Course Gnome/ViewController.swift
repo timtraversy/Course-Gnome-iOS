28cccc4351c464881cfdacb2d79f8437b2c921fc
timtraversy <timtrav@gmail.com>
2017-10-31 01:16:19 -0400

    Initial layout

--- Course Gnome/ViewController.swift
+++ Course Gnome/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var noLoginSearchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        noLoginSearchButton.layer.borderColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 1)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
